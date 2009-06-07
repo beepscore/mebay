8a5243bc3796ccf6626275e96e0f0363c412cd21
beepscore <support@beepscore.com>
2009-06-06 20:54:57 -0700

    Ch 03 pg 144  Must log in to edit or update. Note earlier Rails 2.3.2 had security hole.  Plan to update.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,6 @@
 class AdsController < ApplicationController
+  before_filter :check_logged_in, :only => [:edit, :update]
+
   def new
     @ad = Ad.new
   end
@@ -6,6 +8,16 @@ class AdsController < ApplicationController
   def create
     @ad = Ad.new(params[:ad])
     @ad.save
+    redirect_to "/ads/#{@ad.id}"
+  end
+
+  def edit
+    @ad = Ad.find(params[:id])
+  end
+
+  def update
+    @ad = Ad.find(params[:id])
+    @ad.update_attributes(params[:ad])
     redirect_to "/ads/#{@ad.id}"
   end
 
@@ -17,13 +29,11 @@ class AdsController < ApplicationController
     @ads = Ad.find(:all)
   end
 
-  def edit
-    @ad = Ad.find(params[:id])
-  end
+  private
 
-  def update
-    @ad = Ad.find(params[:id])
-    @ad.update_attributes(params[:ad])
-    redirect_to "/ads/#{@ad.id}"
+  def check_logged_in
+    authenticate_or_request_with_http_basic("Ads") do |username, password|
+      username == "admin" && password == "t4k3th3r3dpill"
+    end
   end
 end
